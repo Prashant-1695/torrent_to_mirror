6bb5d18884f61ade12ac43eb84c27c206c79126c
Prashant-1695 <prashant33968@gmail.com>
2024-10-29 20:43:00 +0530

    add time elapsed and fix zipping name ig
    Signed-off-by: Prashant-1695 <prashant33968@gmail.com>

--- magnet-to-gofile.py
+++ magnet-to-gofile.py
@@ -24,8 +24,8 @@ def download_magnet(magnet_link, download_path):
         'save_path': download_path,
         'storage_mode': lt.storage_mode_t.storage_mode_sparse,
     }
-    handle = lt.add_magnet_uri(ses, magnet_link, params)
 
+    handle = lt.add_magnet_uri(ses, magnet_link, params)
     print(f"Downloading {magnet_link}...")
 
     while not handle.has_metadata():
@@ -48,14 +48,18 @@ def download_magnet(magnet_link, download_path):
     return download_path
 
 def zip_folder(folder_path):
-    zip_file_path = f"{folder_path}.7z"
+    # Get the original folder name
+    folder_name = os.path.basename(os.path.normpath(folder_path))
+    # Create the .7z file path
+    zip_file_path = os.path.join(os.path.dirname(folder_path), f"{folder_name}.7z")
+    # Create the .7z archive
     subprocess.run(['7z', 'a', zip_file_path, folder_path])
     return zip_file_path
 
 def upload_files_to_gofile(file_path):
     url = "https://store1.gofile.io/uploadFile"
-
     links = []
+
     if os.path.isdir(file_path):
         # If it's a directory, zip it first
         zip_file_path = zip_folder(file_path)
@@ -82,7 +86,7 @@ if __name__ == "__main__":
     bot_id = os.environ.get('BOT_ID')  # Set in environment
     chat_id = os.environ.get('CHAT_ID')  # Set in environment
 
-    magnet_link = "magnet:?xt=urn:btih:XS3OX7DC2XSCJQTPCEUFVJ3CRTHGSF2B&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&dn=Kaiju.No.8.S01.2024.JAPANESE.1080p.BluRay.H264.AAC-SWHA"  # Replace with your actual magnet link
+    magnet_link = "magnet:?xt=urn:btih:IZAXKUIE4T5DO6RAYDJ2BIWZDBSZNPOB&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&dn=%5BEMBER%5D%20My%20Hero%20Academia%20%282024%29%20%28Season%207%29%20%5B1080p%5D%20%5BDual%20Audio%20HEVC%20WEBRip%5D%20%28Boku%20no%20Hero%20Academia%207th%20Season%29%20%28Batch%29"  # Replace with your actual magnet link
     download_path = "./downloads/"  # Folder to save downloaded files
 
     if not os.path.exists(download_path):
